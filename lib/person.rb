a4665d6fae77199bc43aba34372df332ef6cc755
Anna Schmidt <marshall1697@gmail.com>
2022-10-11 14:42:19 -0600

    commit tree 3095bfd2a328434ae1ad62b930f29c76d1329798

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
+    #setter
+    def name=(name)
+        @name = name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    #getter
+    def name
+        @name
+    end
+
+    def job
+        @job
+    end
 
 end
